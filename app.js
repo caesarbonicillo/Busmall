dd712cda1a8589cdd5dda54642ba8e8cf364cb49
David Bonicillo <caesarbonicillo@gmail.com>
2018-09-25 00:05:47 -0700

    got three items to pop up. randomized but I can't figure out how to splice

--- app.js
+++ app.js
@@ -2,26 +2,43 @@
 
 
 Product.allProducts = []; // this is my product list of picture Items
-//var imgElement = document.getElementById('product');
-// var imgElement = document.getElementById('productTwo');
-var imgElement = document.getElementById('productThree');
+var imgElement = document.getElementById('product'); 
+var imgElementTwo = document.getElementById('productTwo');
+var imgElementThree = document.getElementById('productThree');
 
 function Product(filePath, productName){ 
-this.imgSource = filePath;
-this.productName = productName;
-
-Product.allProducts.push(this);
+  this.imgSource = filePath;
+  this.productName = productName;
+  Product.allProducts.push(this);
 }
 
-
-function randomProduct(){
+Product.prototype.randomNumber = function (randomNumber) //I THINK I HAVE TO USE A PROTOTYPE HERE
+{
+function randomProduct()
+{
   var randomNumber = Math.floor(Math.random()* Product.allProducts.length);
   imgElement.src = Product.allProducts[randomNumber].imgSource;
   imgElement.alt = Product.allProducts[randomNumber].productName;
-  console.log(randomProduct);
 }
 
+function randomProductTwo()
+{
+  var randomNumberTwo = Math.floor(Math.random()* Product.allProducts.length);
+  imgElementTwo.src = Product.allProducts[randomNumberTwo].imgSource;
+  imgElementTwo.alt = Product.allProducts[randomNumberTwo].productName;
+  
+} 
+
+function randomProductThree()
+{
+  var randomNumberThree = Math.floor(Math.random()* Product.allProducts.length);
+  imgElementThree.src = Product.allProducts[randomNumberThree].imgSource;
+  imgElementThree.alt = Product.allProducts[randomNumberThree].productName;
+}
+}
 imgElement.addEventListener('click', randomProduct)
+imgElementTwo.addEventListener('click', randomProductTwo)
+imgElementThree.addEventListener('click', randomProductThree)
 
 new Product ('img/bag.jpg', 'bag'); // new instancating a new object
 new Product ('img/banana.jpg', 'banana');
@@ -31,7 +48,7 @@ new Product ('img/breakfast.jpg', 'breakfast');
 new Product ('img/bubblegum.jpg', 'bubblegum');
 new Product ('img/chair.jpg', 'chair');
 new Product ('img/cthulhu.jpg', 'cthulhu.jpg');
-new Product ('img/dog-duckk.jpg', 'dog-duck');
+new Product ('img/dog-duck.jpg', 'dog-duck');
 new Product ('img/dragon.jpg', 'dragon');
 new Product ('img/pen.jpg', 'pen');
 new Product ('img/pet-sweep.jpg', 'pet-sweep');
@@ -43,4 +60,7 @@ new Product ('img/usb.gif', 'usb.gif');
 new Product ('img/water-can.jpg', 'water-can');
 new Product ('img/wine-glass.jpg', 'wine-glass');
 
+
 randomProduct();
+randomProductTwo();
+randomProductThree();
